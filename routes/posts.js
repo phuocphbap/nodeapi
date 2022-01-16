7f605b7ecfe091129da2cb4d49dade42df4926eb
phuocphbap <phuocph@bap.jp>
2022-01-16 15:18:11 +0700

    init

---
+++ routes/posts.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const PostModel = require("../models/Post");
+
+router.get("/", async (req, res) => {
+  try {
+    console.log("get ok");
+    const posts = await PostModel.find();
+    res.json(posts);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    let post = new PostModel({ title: req.body.title });
+    let data = await post.save();
+    res.json(data);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+module.exports = router;
